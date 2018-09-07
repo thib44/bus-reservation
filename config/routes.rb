a0bc8f1e19fef04a79f8ea4e4cb2fff3770345a1
thib44 <thibault.finot@gmail.com>
2018-09-07 15:57:09 +0200

    add update account

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
 
   root 'bus_reservations#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
